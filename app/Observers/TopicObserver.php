f855bdbb2bb2191ab6433152a7578a1d7ad9e37c
fthvgb1 <fthvgb1@163.com>
2018-01-02 01:37:18 +0800

    添加队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,8 +2,8 @@
 
 namespace App\Observers;
 
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
-use App\Tools\SlugTranslateHandler;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -19,9 +19,13 @@ class TopicObserver
     {
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+    }
+
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
     }
 
