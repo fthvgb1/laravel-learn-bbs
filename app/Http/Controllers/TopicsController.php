85291df5b377433817ca910d61f6b2b0b19d7a20
fthvgb1 <fthvgb1@163.com>
2018-01-01 14:51:46 +0800

    话题列表

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,7 +14,7 @@ class TopicsController extends Controller
 
     public function index()
     {
-        $topics = Topic::paginate();
+        $topics = Topic::with('user', 'category')->paginate();
         return view('topics.index', compact('topics'));
     }
 
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,4 +5,10 @@ namespace App\Models;
 class Category extends BaseModel
 {
     protected $guarded = ['id'];
+
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
