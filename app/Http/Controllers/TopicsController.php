7db8c91e69708e8c472e891ea12946392b2dff20
fthvgb1 <fthvgb1@163.com>
2018-01-01 20:18:33 +0800

    防xss

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -38,7 +38,7 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('success', '添加成功！.');
     }
 
     public function edit(Topic $topic)
@@ -75,7 +75,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('success', '编辑成功！.');
     }
 
     public function destroy(Topic $topic)
